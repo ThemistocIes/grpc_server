4c80a6aa96217e6189ad75eba9834f58cba02951
ThemistocIes <captainjackvorobey1@gmail.com>
2024-02-06 13:29:36 +0700

    refactor part 3

--- src/main/java/home/work/grpc_server/GrpcServerApplication.java
+++ src/main/java/home/work/grpc_server/GrpcServerApplication.java
@@ -2,9 +2,8 @@ package home.work.grpc_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class GrpcServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcServerApplication.class, args);
