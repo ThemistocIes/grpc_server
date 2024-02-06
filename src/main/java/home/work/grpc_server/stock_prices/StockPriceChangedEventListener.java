ad0fb5d7a5110fb9e604cd7bd2ac630cc9aadcff
ThemistocIes <captainjackvorobey1@gmail.com>
2024-02-06 19:48:00 +0700

    refactor part 5

--- src/main/java/home/work/grpc_server/stock_prices/StockPriceChangedEventListener.java
+++ src/main/java/home/work/grpc_server/stock_prices/StockPriceChangedEventListener.java
@@ -3,6 +3,7 @@ package home.work.grpc_server.stock_prices;
 import com.google.common.eventbus.Subscribe;
 
 public class StockPriceChangedEventListener {
+    //TODO DIVIDE A LOGIC UPDATING STOCK PRICES AND LISTENING EVENTS
     @Subscribe
     public void handleEvent(StockPriceChangedEvent event) {
         System.out.println(event.toString());
