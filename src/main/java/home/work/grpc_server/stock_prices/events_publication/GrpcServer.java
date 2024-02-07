cbd0217a8778ff8427d3368e178dabae7ea41fcc
ThemistocIes <captainjackvorobey1@gmail.com>
2024-02-07 19:27:34 +0700

    refactor part 8

--- src/main/java/home/work/grpc_server/stock_prices/events_publication/GrpcServer.java
+++ src/main/java/home/work/grpc_server/stock_prices/events_publication/GrpcServer.java
@@ -1,30 +1,23 @@
 package home.work.grpc_server.stock_prices.events_publication;
 
 import home.work.grpc_server.stock_prices.RandomStockPriceUpdatingTaskService;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.concurrent.Executors;
 
 @Slf4j
-@GrpcService
+@Service
 public class GrpcServer {
+    @Value("${grpc.server.port}")
+    private String grpcServerPort;
+
     @Bean
-    private void runGrpcServer() throws IOException, InterruptedException {
+    private void runGrpcServer() {
         DomainEvents.subscribe(new StockPriceChangedEventListener());
         Executors.newSingleThreadExecutor().submit(new RandomStockPriceUpdatingTaskService());
-
-        Server server = ServerBuilder
-                .forPort(8080)
-                .addService(new PriceService())
-                .build();
-
-        server.start();
-        log.info("gRPC server started, listening on port: {}", server.getPort());
-        server.awaitTermination();
+        log.info("gRPC server started, listening on port: {}", grpcServerPort);
     }
 }
--- src/main/java/home/work/grpc_server/stock_prices/events_publication/StockPriceService.java
+++ src/main/java/home/work/grpc_server/stock_prices/events_publication/StockPriceService.java
@@ -8,8 +8,10 @@ import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
-public class PriceService extends StockPriceServiceGrpc.StockPriceServiceImplBase {
+@GrpcService
+public class StockPriceService extends StockPriceServiceGrpc.StockPriceServiceImplBase {
     private final StockRepository repository = StockRepository.INSTANCE;
     private final StockPriceChangedSubject subject = StockPriceChangedSubject.INSTANCE;
 
